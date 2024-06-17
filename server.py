ae88060ddcd89dd4d6470c53c56ef720df168188
amanley97 <amanley97@ku.edu>
2024-06-17 18:23:48 -0500

    update to run simulation method

--- server.py
+++ server.py
@@ -1,13 +1,13 @@
 # gEMA/server.py
 # Runs the gEMA backend and awaits external commands.
 
-import sys, os
+import sys, os, copy
 from http.server import HTTPServer
 from .handler import BackendHandler
 from .obtain import *
 
 
-class BackendServer:
+class gEMAServer:
     def __init__(self, port=5000):
         self.port = port
         self.config = None
@@ -29,35 +29,24 @@ class BackendServer:
         httpd = HTTPServer(server_address, self.handler)
 
         print(f"""
-               Starting server on port {self.port}.
-               For help, access /help on the server URL or consult the documentation.
-               """)
+        Starting server on port {self.port}.
+        For help, access /help on the server URL or consult the documentation.
+        """)
         httpd.serve_forever()
 
     def run_gem5_simulator(self, id):
-        with open("./m5out/stats.txt", "r+") as file:
-            file.seek(0)
-            file.truncate()
-
-        with open(f"m5out/output{id}.txt", "w") as f:
-            sys.stdout = f
-            sys.stderr = f
-            print(f"Simulation ID: {id} PID: {os.getpid()}")
-            self.config = self.handler.saved_configs.get(id)
-            if self.config is None:
-                print("Failed to obtain config.")
-            else:
-                print(f"Received config for id: {id}")
-                simulator = Simulator(board=self.config)
-                simulator.run()
-                print(
-                    "Exiting @ tick {} because {}.".format(
-                        simulator.get_current_tick(), simulator.get_last_exit_event_cause()
-                    )
-                )
-                sys.stdout.flush()
-                sys.stderr.flush()
-
+        print(f"Simulation ID: {id} PID: {os.getpid()}")
+        print(self.handler.saved_configs.get(id))
+        self.config = copy.deepcopy(self.handler.saved_configs.get(id)) #TODO: Fix this so that the same config can be run many times
+        if self.config is None:
+            print("Failed to obtain config.")
+        else:
+            print(f"Received config for id: {id}")
+            simulator = Simulator(board=self.config)
+            simulator.run()
+            print(f"Exiting @ tick {simulator.get_current_tick()} because {simulator.get_last_exit_event_cause()}.")
+        del simulator
+        del self.config
+        print(self.handler.saved_configs.get(id))
         dump()
         reset()
-        m5.statsreset()
