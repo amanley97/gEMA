3fd2eaa197597c8a0f21d9f5a94fe3fb9c45ba7a
amanley97 <amanley97@ku.edu>
2024-06-18 17:42:55 -0500

    create class for server

--- server.py
+++ server.py
@@ -1,52 +1,21 @@
 # gEMA/server.py
 # Runs the gEMA backend and awaits external commands.
 
-import sys, os, copy
-from http.server import HTTPServer
-from .handler import BackendHandler
-from .obtain import *
-
+from http.server import HTTPServer as gEMAHTTP
+from .handler import gEMAHandler
 
 class gEMAServer:
     def __init__(self, port=5000):
         self.port = port
         self.config = None
-        self.handler = self.handler_factory()
-
-    def handler_factory(self):
-        # Factory function to create a handler with access to the server instance
-        server_instance = self
-
-        class CustomHandler(BackendHandler):
-            def __init__(self, *args, **kwargs):
-                self.server_instance = server_instance
-                super().__init__(*args, **kwargs)
-
-        return CustomHandler
+        self.handler = gEMAHandler
 
     def run_server(self):
         server_address = ("", self.port)
-        httpd = HTTPServer(server_address, self.handler)
+        httpd = gEMAHTTP(server_address, self.handler)
 
         print(f"""
         Starting server on port {self.port}.
         For help, access /help on the server URL or consult the documentation.
         """)
         httpd.serve_forever()
-
-    def run_gem5_simulator(self, id):
-        print(f"Simulation ID: {id} PID: {os.getpid()}")
-        print(self.handler.saved_configs.get(id))
-        self.config = copy.deepcopy(self.handler.saved_configs.get(id)) #TODO: Fix this so that the same config can be run many times
-        if self.config is None:
-            print("Failed to obtain config.")
-        else:
-            print(f"Received config for id: {id}")
-            simulator = Simulator(board=self.config)
-            simulator.run()
-            print(f"Exiting @ tick {simulator.get_current_tick()} because {simulator.get_last_exit_event_cause()}.")
-        del simulator
-        del self.config
-        print(self.handler.saved_configs.get(id))
-        dump()
-        reset()
