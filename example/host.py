3bc022cdd03f0426b07e0f68d2477c880c4942a3
amanley97 <amanley97@ku.edu>
2024-06-18 17:40:22 -0500

    added new config examples

--- example/host.py
+++ example/host.py
@@ -1,16 +1,26 @@
 # gEMA/example/host.py
 # Example host to run gEMA instance.
 
-import argparse
-
-parser = argparse.ArgumentParser("gEMA host")
-parser.add_argument("port", help="Port to use for API", type=int)
-args = parser.parse_args()
+# import argparse
+from gem5.gEMA.configure import gEMAConfigurator
+from gem5.utils.multiprocessing import Process
+from gem5.gEMA import gEMAServer
+# parser = argparse.ArgumentParser("gEMA host")
+# parser.add_argument("port", help="Port to use for API", type=int)
+# args = parser.parse_args()
 
 if __name__ == '__m5_main__':
-    try:
-        from gem5.gEMA import gEMAServer
-        s = gEMAServer(port=args.port)
-        s.run_server()
-    except ImportError:
-        print("Failed to import gEMA")
+    # try:
+    #     from gem5.gEMA import gEMAServer
+    #     s = gEMAServer(port=args.port)
+    #     s.run()
+    # except ImportError:
+    #     print("Failed to import gEMA")
+    # cfg = gEMAConfigurator()
+    server = gEMAServer()
+    main = Process(target=server.run_server)
+    # sub = Process(target=cfg.run_gem5_simulator, args=(1,))
+    main.start()
+    # sub.start()
+    main.join()
+    # sub.join()
